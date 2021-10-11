11a682ec260921382d40d63fc3a07b154e2548b3
BSevault <b.sevault@gmail.com>
2021-10-11 22:43:09 +0200

    Ajout animation d'affichage du contenu

---
+++ public/scripts/main.js
@@ -0,0 +1,13 @@
+// var toHide;
+// var toDisplay;
+// var content_list = document.getElementsByClassName("content");
+// for(var i=0; i<content_list.length; i++){
+//     // content_list[i].style.display = "none";
+// }
+
+// function getTarget(event){
+
+//     console.log(event.target);
+//     console.log(event);
+
+// }
