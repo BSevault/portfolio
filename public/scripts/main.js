68ea3fde34cf83fe4235d7d492d35919ef76bccc
BSevault <b.sevault@gmail.com>
2021-12-17 14:45:11 +0100

    project reset

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,12 @@
-// var toHide;
-// var toDisplay;
-// var content_list = document.getElementsByClassName("content");
-// for(var i=0; i<content_list.length; i++){
-//     // content_list[i].style.display = "none";
-// }
-
-// function getTarget(event){
-
-//     console.log(event.target);
-//     console.log(event);
-
-// }
+var starter = document.getElementById("start");
+let loaded = false;
+function firstLoad(){
+    if(loaded == false){
+        starter.style.display = "block";
+        loaded = true;
+    }else if(loaded==false){
+        starter.style.display = "none";
+    }
+}
+starter.addEventListener('load', firstLoad());
+    
