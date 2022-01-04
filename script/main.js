3dd5dde1c8e724614e028ad5752ad5dac4f6c712
BSevault <b.sevault@gmail.com>
2022-01-04 11:56:58 +0100

    added accordion

--- script/main.js
+++ script/main.js
@@ -1,65 +1,52 @@
-/** Changement d'aspect navbar quand on scroll depuis l'accueil */
-window.addEventListener("scroll", () => {
-    var header = document.getElementById("header");
-    header.classList.toggle("sticky", window.scrollY > 0);
-});
-
 /** Toggle du menu burger mobile */
 let toggleMenu = () => {
-    let menuToggle = document.getElementById("hamburger");
-    let menu = document.getElementById("menu");
-    menuToggle.classList.toggle("active");
-    menu.classList.toggle("active");
+    let menuToggle = document.querySelector('#hamburger');
+    let menu = document.querySelector('#menu');
+    menuToggle.classList.toggle('active');
+    menu.classList.toggle('active');
 };
 
-/** Surbrillance navbar en fonction de la section à l'écran */
-const sections = document.querySelectorAll("section");
-const navLi = document.querySelectorAll("header ul#menu li");
-window.addEventListener("scroll", () => {
-    let current = "";
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (scrollY >= sectionTop - sectionHeight / sections.length) {
-            current = section.getAttribute("id");
-        }
+document.addEventListener("DOMContentLoaded", () => {
+    
+    /** Changement d'aspect navbar quand on scroll depuis l'accueil */
+    window.addEventListener("scroll", () => {
+        var header = document.getElementById("header");
+        header.classList.toggle("sticky", window.scrollY > 0);
+    });
+    
+    /** Surbrillance navbar en fonction de la section à l'écran */
+    const sections = document.querySelectorAll("section");
+    const navLi = document.querySelectorAll("header ul#menu li");
+    window.addEventListener("scroll", () => {
+        let current = "";
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (scrollY >= sectionTop - sectionHeight / sections.length) {
+                current = section.getAttribute("id");
+            }
+        });
+
+        navLi.forEach( li => {
+            li.classList.remove("active");
+            if (li.classList.contains(current)) {
+                li.classList.add("active");
+            }
+        });
     });
 
-    navLi.forEach( li => {
-        li.classList.remove("active");
-        if (li.classList.contains(current)) {
-            li.classList.add("active");
-        }
-    });
+    /** Accordéon */
+    const btns = document.querySelectorAll(".acc-btn");
+    function accordion() {
+    this.classList.toggle("is-open");
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight) content.style.maxHeight = null;
+    else content.style.maxHeight = content.scrollHeight + "px";
+  }
+  
+  // event
+  btns.forEach((el) => el.addEventListener("click", accordion));
+  
+  
+
 });
-
-/** Toggle d'un élément réalisation */
-const toggleOpen = (box) => {
-    box.addEventListener ("click", (element) => {
-        // console.log(element.target);
-        // console.log(element.currentTarget);
-        let clickedElement = element.currentTarget;
-        clickedElement.classList.toggle("open");
-        console.log(clickedElement.classList);
-    });
-}
-
-const workBoxes = document.querySelectorAll(".workBx");
-workBoxes.forEach( box => {
-    toggleOpen(box);
-});
-
-// box.onclick = element => {
-//     let clickedElement = element.target; // to get the element
-//     console.log(clickedElement.currentTarget);
-//     if (clickedElement.classList.value == "workBx") {
-//         clickedElement.classList.toggle("open");
-//     }
-// }
-
-// window.onclick = (element) => {
-//     let clickedElement = element.target; // to get the element
-//     console.log(clickedElement.classList);
-//     clickedElement.classList.toggle("open");
-//     console.log(clickedElement.classList);
-// };
